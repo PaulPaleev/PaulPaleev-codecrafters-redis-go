2716abe337417b0b1686f3fc8d9fe75d3365f4a6
Pavel <pavel.panteleev@vrpconsulting.com>
2025-04-11 15:54:30 +0300

    mult pongs same conn

--- app/main.go
+++ app/main.go
@@ -13,10 +13,12 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+
 	defer l.Close()
+	conn, err := l.Accept()
 
 	for {
-		conn, err := l.Accept()
+
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
